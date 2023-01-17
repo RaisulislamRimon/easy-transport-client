80a83a9257fd7c21f4f4ce9c03a819786d95c6a8
wpmdalamin <mdalamin7g@gmail.com>
2023-01-17 17:09:13 +0600

    footer and safety section design done

--- src/Pages/Home/Home/Home.js
+++ src/Pages/Home/Home/Home.js
@@ -1,10 +1,12 @@
 import React from "react";
 import Banner from "../Banner/Banner";
+import Safety from "../Safety/Safety";
 
 const Home = () => {
   return (
     <div>
       <Banner />
+      <Safety></Safety>
     </div>
   );
 };
