6dc23d38da8fee1a06e85bb635497923922ef7b4
MD. Raisul Islam Rimon <51745662+RaisulislamRimon@users.noreply.github.com>
2023-01-26 08:06:25 +0000

    added checkout & payment

---
+++ src/Pages/Dashboard/Checkout/Checkout.js
@@ -0,0 +1,170 @@
+import { CardElement, useElements, useStripe } from "@stripe/react-stripe-js";
+import React, { useEffect, useState } from "react";
+import Swal from "sweetalert2";
+
+const CheckoutForm = ({ info }) => {
+  const [cardError, setCardError] = useState("");
+  const [success, setSuccess] = useState("");
+  const [processing, setProcessing] = useState(false);
+  const [transactionId, setTransactionId] = useState("");
+  const [clientSecret, setClientSecret] = useState("");
+
+  const stripe = useStripe();
+  const elements = useElements();
+  console.log(info);
+  const { bookData, email, name, _id } = info;
+  const { resalePrice } = bookData;
+  console.log(resalePrice);
+  useEffect(() => {
+    // Create PaymentIntent as soon as the page loads
+    fetch(
+      "vercel server api link/create-payment-intent",
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          // authorization: `bearer ${localStorage.getItem("accessToken")}`,
+        },
+        body: JSON.stringify({ resalePrice }),
+      }
+    )
+      .then((res) => res.json())
+      .then((data) => setClientSecret(data.clientSecret));
+  }, [resalePrice]);
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+
+    if (!stripe || !elements) {
+      return;
+    }
+
+    const card = elements.getElement(CardElement);
+    if (card === null) {
+      return;
+    }
+
+    const { error, paymentMethod } = await stripe.createPaymentMethod({
+      type: "card",
+      card,
+    });
+
+    if (error) {
+      console.log(error);
+      setCardError(error.message);
+    } else {
+      setCardError("");
+    }
+    setSuccess("");
+    setProcessing(true);
+    const { paymentIntent, error: confirmError } =
+      await stripe.confirmCardPayment(clientSecret, {
+        payment_method: {
+          card: card,
+          billing_details: {
+            name: name,
+            email: email,
+          },
+        },
+      });
+
+    console.log(paymentIntent);
+
+    if (confirmError) {
+      setCardError(confirmError.message);
+      return;
+    }
+    if (paymentIntent.status === "succeeded") {
+      console.log("card info", card);
+      // store payment info in the database
+      const payment = {
+        resalePrice,
+        transactionId: paymentIntent.id,
+        email,
+        orderInfoId: _id,
+      };
+      console.log(payment);
+      fetch("vercel server api link/payments", {
+        method: "POST",
+        headers: {
+          "content-type": "application/json",
+        },
+        body: JSON.stringify(payment),
+      })
+        .then((res) => res.json())
+        .then((data) => {
+          console.log(data);
+          if (data.insertedId) {
+            setSuccess("Congrats! your payment completed");
+            setTransactionId(paymentIntent.id);
+          }
+        });
+
+      fetch(
+        `vercel server api link/makeorderpaid/${_id}`,
+        {
+          method: "PUT",
+          headers: {
+            "content-type": "application/json",
+          },
+          body: JSON.stringify({ isPaid: true }),
+        }
+      )
+        .then((res) => res.json())
+        .then((data) => {
+          if (data.acknowledged) {
+            Swal.fire({
+              title: "Congrats!",
+              text: "Your order is placed successfully",
+              icon: "success",
+              showConfirmButton: false,
+              timer: 2000,
+            });
+          }
+        });
+    }
+    setProcessing(false);
+  };
+
+  return (
+    <div className="grid grid-flow-col my-10">
+      <form onSubmit={handleSubmit}>
+        <CardElement
+          options={{
+            style: {
+              base: {
+                fontSize: "16px",
+                color: "#424770",
+                "::placeholder": {
+                  color: "#aab7c4",
+                },
+              },
+              invalid: {
+                color: "#9e2146",
+              },
+            },
+          }}
+        />
+        <button
+          className="btn btn-md mt-4 btn-primary"
+          type="submit"
+          disabled={!stripe || !clientSecret || processing}
+        >
+          Pay
+        </button>
+      </form>
+      <p className="text-red-500">{cardError}</p>
+      {success && (
+        <div>
+          <p className="text-green-500">{success}</p>
+          <p>
+            Your transactionId:{" "}
+            <span className="font-bold">{transactionId}</span>
+          </p>
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default CheckoutForm;
---
+++ src/Pages/Dashboard/Payment/Payment.js
@@ -0,0 +1,59 @@
+import { Elements } from "@stripe/react-stripe-js";
+import { loadStripe } from "@stripe/stripe-js";
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+import { useLoaderData, useParams } from "react-router-dom";
+import CheckoutForm from "../Checkout/Checkout";
+
+const stripePromise = loadStripe(process.env.REACT_APP_STRIPE_PK);
+
+const Payment = () => {
+  const { myOrderId } = useParams();
+  const data = useLoaderData();
+  const [orderInfo, setOrderInfo] = useState(data);
+  console.log(orderInfo);
+  const [error, setError] = useState("");
+  // fetch(`vercel server api link/my-orders/${myOrderId}`)
+  //   .then((res) => res.json())
+  //   .then((data) => {
+  //     // setInfo(data);
+  //     console.log(data);
+  //     setInfo(data);
+  //   });
+  // useEffect(() => {
+  //   axios
+  //     .get(`vercel server api link/my-orders/${myOrderId}`)
+  //     .then(function (response) {
+  //       // handle success
+  //       console.log(response.data);
+  //       setInfo(response.data);
+  //     })
+  //     .catch(function (error) {
+  //       // handle error
+  //       setError(error.message);
+  //     });
+  // }, [myOrderId]);
+  console.log("info state : ", orderInfo);
+  return (
+    <div>
+      <h1 className="text-3xl text-center my-4 underline">
+        Payment ({orderInfo?.bookData?.book_name})
+      </h1>
+
+      {error && (
+        <div className="text-2xl text-red-600 min-h-screen justify-center items-center">
+          <h1>{error}</h1>
+        </div>
+      )}
+
+      <div>
+        <Elements stripe={stripePromise}>
+          <CheckoutForm info={orderInfo} />
+          {/* <CheckoutForm info={data} /> */}
+        </Elements>
+      </div>
+    </div>
+  );
+};
+
+export default Payment;
