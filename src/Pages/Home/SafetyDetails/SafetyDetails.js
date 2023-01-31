7e9db7e22227d759bad3d4e45c7aac31d5ec64b0
MD. Raisul Islam Rimon <51745662+RaisulislamRimon@users.noreply.github.com>
2023-01-31 07:47:24 +0000

    update safety banner

--- src/Pages/Home/SafetyDetails/SafetyDetails.js
+++ src/Pages/Home/SafetyDetails/SafetyDetails.js
@@ -5,19 +5,19 @@ const SafetyDetails = () => {
     <div>
       {/* safety banner */}
       <div className="card lg:card-side bg-base-100 shadow-xl">
-        <iframe
-          width="640"
-          height="360"
-          frameborder="0"
-          src="https://mega.nz/embed/bYdjlahB#A9t2PYb5KB7N44ehuNJmmgXsk03UseUqTQbI5NV-jXc!1a1m"
-          allowfullscreen
-          allow="autoplay;"
-          title="safety-banner"
-        ></iframe>
+        <img
+          src="https://i.ibb.co/4Y0HwX5/ezgif-com-gif-maker.gif"
+          alt="safety_banner"
+        />
 
         <div className="card-body">
-          <h2 className="card-title">New album is released!</h2>
-          <p>Click the button to listen on Spotiwhy app.</p>
+          <h1 className="card-title text-3xl">Our commitment to safety</h1>
+          <p>
+            We want you to move freely, make the most of your time, and be
+            connected to the people and places that matter most to you. That’s
+            why we are committed to safety, from the creation of new standards
+            to the development of technology with the aim of reducing incidents.
+          </p>
           <div className="card-actions justify-end">
             <button className="btn btn-primary">Listen</button>
           </div>
