665507e2f1ac57c6b6075de1d6b352b15f7e7a6c
MD. Raisul Islam Rimon <51745662+RaisulislamRimon@users.noreply.github.com>
2023-02-09 09:56:25 +0000

    button design update

--- src/Pages/Home/Category/Category.js
+++ src/Pages/Home/Category/Category.js
@@ -22,7 +22,7 @@ const Category = ({ cars }) => {
                 className=" inline-block px-6 py-2.5 bg-blue-600 text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out"
               >
                 {" "}
-                <Link to={`/services/${_id}`}>See all</Link>
+                <Link to={`/services/${_id}`} className="text-white hover:text-white hover:no-underline">See all</Link>
               </button>
             </div>
           </div>
