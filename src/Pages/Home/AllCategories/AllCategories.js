5efb84fd6dbd11956adbbeaaa4622362ebafe2de
MD. Raisul Islam Rimon <51745662+RaisulislamRimon@users.noreply.github.com>
2023-02-09 11:38:46 +0000

    added search bar in allcategory page

--- src/Pages/Home/AllCategories/AllCategories.js
+++ src/Pages/Home/AllCategories/AllCategories.js
@@ -22,6 +22,36 @@ const AllCategories = () => {
       <div>
         <h2 className="text-center my-5">All Cetagories</h2>
       </div>
+
+      {/* search bar */}
+      <div className="flex justify-end mt-5 mb-10 mr-10">
+        <div className="form-control">
+          <div className="input-group">
+            <input
+              type="text"
+              placeholder="Search…"
+              className="input input-bordered"
+            />
+            <button className="btn btn-square">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="h-6 w-6"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
+                />
+              </svg>
+            </button>
+          </div>
+        </div>
+      </div>
+
       <div className="grid gap-3 lg:grid-cols-3 md:grid-cols-2 grid-cols-1">
         {/* <div className="card w-96 bg-base-100 shadow-xl">
         <h2>All Cetagories</h2>
