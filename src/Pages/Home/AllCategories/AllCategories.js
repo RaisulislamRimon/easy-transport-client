3c9fa451264492d4c550b1b6b427b72073febd73
MD. Raisul Islam Rimon <51745662+RaisulislamRimon@users.noreply.github.com>
2023-02-09 11:30:44 +0000

    update button name

--- src/Pages/Home/AllCategories/AllCategories.js
+++ src/Pages/Home/AllCategories/AllCategories.js
@@ -19,69 +19,69 @@ const AllCategories = () => {
 
   return (
     <div>
-    <div>
-      <h2>All Cetagories</h2>
-    </div>
-    <div className="grid gap-3 lg:grid-cols-3 md:grid-cols-2 grid-cols-1">
-      {/* <div className="card w-96 bg-base-100 shadow-xl">
+      <div>
+        <h2 className="text-center my-5">All Cetagories</h2>
+      </div>
+      <div className="grid gap-3 lg:grid-cols-3 md:grid-cols-2 grid-cols-1">
+        {/* <div className="card w-96 bg-base-100 shadow-xl">
         <h2>All Cetagories</h2>
       </div> */}
-      <div className="card w-96 bg-base-100 shadow-xl">
-        <figure>
-          <img src={service?.product2[1]?.picture} alt="Shoes" />
-        </figure>
-        <div className="card-body">
-          <h2 className="card-title">{service?.product2[1]?.name}</h2>
-          <div className="card-actions justify-end">
-            <button className="btn btn-primary">Book Now</button>
+        <div className="card w-96 bg-base-100 shadow-xl">
+          <figure>
+            <img src={service?.product2[1]?.picture} alt="Shoes" />
+          </figure>
+          <div className="card-body">
+            <h2 className="card-title">{service?.product2[1]?.name}</h2>
+            <div className="card-actions justify-end">
+              <button className="btn btn-primary">Ride Now</button>
+            </div>
+          </div>
+        </div>
+        <div className="card w-96 bg-base-100 shadow-xl">
+          <figure>
+            <img src={service?.product3[2]?.picture} alt="Shoes" />
+          </figure>
+          <div className="card-body">
+            <h2 className="card-title">{service?.product3[2]?.name}</h2>
+            <div className="card-actions justify-end">
+              <button className="btn btn-primary">Ride Now</button>
+            </div>
+          </div>
+        </div>
+        <div className="card w-96 bg-base-100 shadow-xl">
+          <figure>
+            <img src={service?.product4[3]?.picture} alt="Shoes" />
+          </figure>
+          <div className="card-body">
+            <h2 className="card-title">{service?.product4[3]?.name}</h2>
+            <div className="card-actions justify-end">
+              <button className="btn btn-primary">Ride Now</button>
+            </div>
+          </div>
+        </div>
+        <div className="card w-96 bg-base-100 shadow-xl">
+          <figure>
+            <img src={service?.product5[4]?.picture} alt="Shoes" />
+          </figure>
+          <div className="card-body">
+            <h2 className="card-title">{service?.product5[4]?.name}</h2>
+            <div className="card-actions justify-end">
+              <button className="btn btn-primary">Ride Now</button>
+            </div>
+          </div>
+        </div>
+        <div className="card w-96 bg-base-100 shadow-xl">
+          <figure>
+            <img src={service?.product6[5]?.picture} alt="Shoes" />
+          </figure>
+          <div className="card-body">
+            <h2 className="card-title">{service?.product6[5]?.name}</h2>
+            <div className="card-actions justify-end">
+              <button className="btn btn-primary">Ride Now</button>
+            </div>
           </div>
         </div>
       </div>
-      <div className="card w-96 bg-base-100 shadow-xl">
-        <figure>
-          <img src={service?.product3[2]?.picture} alt="Shoes" />
-        </figure>
-        <div className="card-body">
-          <h2 className="card-title">{service?.product3[2]?.name}</h2>
-          <div className="card-actions justify-end">
-            <button className="btn btn-primary">Book Now</button>
-          </div>
-        </div>
-      </div>
-      <div className="card w-96 bg-base-100 shadow-xl">
-        <figure>
-          <img src={service?.product4[3]?.picture} alt="Shoes" />
-        </figure>
-        <div className="card-body">
-          <h2 className="card-title">{service?.product4[3]?.name}</h2>
-          <div className="card-actions justify-end">
-            <button className="btn btn-primary">Book Now</button>
-          </div>
-        </div>
-      </div>
-      <div className="card w-96 bg-base-100 shadow-xl">
-        <figure>
-          <img src={service?.product5[4]?.picture} alt="Shoes" />
-        </figure>
-        <div className="card-body">
-          <h2 className="card-title">{service?.product5[4]?.name}</h2>
-          <div className="card-actions justify-end">
-            <button className="btn btn-primary">Book Now</button>
-          </div>
-        </div>
-      </div>
-      <div className="card w-96 bg-base-100 shadow-xl">
-        <figure>
-          <img src={service?.product6[5]?.picture} alt="Shoes" />
-        </figure>
-        <div className="card-body">
-          <h2 className="card-title">{service?.product6[5]?.name}</h2>
-          <div className="card-actions justify-end">
-            <button className="btn btn-primary">Book Now</button>
-          </div>
-        </div>
-      </div>
-    </div>
     </div>
   );
 };
